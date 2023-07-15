84b98ea5e211b0ed698fe9c1db5d3fa366deefb2
ibrahim akdemir <ibrakdemir1997@gmail.com>
2023-07-15 22:43:18 +0300

    Admin's views and models were added

---
+++ src/WebMVC/WebUI/GraduateAppProject.MVC/Models/AdminInformationsModel.cs
@@ -0,0 +1,10 @@
+﻿using GraduateAppProject.DataTransferObjects.Responses.UserResponses;
+
+namespace GraduateAppProject.MVC.Models
+{
+    public class AdminInformationsModel
+    {
+        public UserIdentityInformationDTO? UserIdentityInformationDTO { get; set; }
+        public UserContactInformationDTO? UserContactInformationDTO { get; set; }
+    }
+}
